04f6ab7b4dee37eeb1a7b37f80f26f6809e75596
YeniNAzizah <yeni_azizah_24rpl@student.smktelkom-mlg.sch.id>
2016-10-09 21:13:24 +0700

    Livecycle Music

--- app/src/main/java/id/sch/smktelkom_mlg/learn/activitylivecycle/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/activitylivecycle/MainActivity.java
@@ -1,5 +1,6 @@
 package id.sch.smktelkom_mlg.learn.activitylivecycle;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -7,6 +8,7 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "LivecycleTag";
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate: ");
 
-
+        mediaPlayer = MediaPlayer.create(this, R.raw.lagu);
     }
 
     @Override
@@ -28,12 +30,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume: ");
+        mediaPlayer.start();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.d(TAG, "onPause: ");
+        mediaPlayer.pause();
     }
 
     @Override
@@ -52,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy:");
+        if (mediaPlayer != null)
+            mediaPlayer.release();
     }
 }
 
